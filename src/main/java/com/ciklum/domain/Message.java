f1b38873cda7f9c20f92bdddf868935c57360adf
olegnikitindev <olegnikitindev@gmail.com>
2016-08-04 22:58:00 +0300

    Added domain objects

--- src/main/java/com/ciklum/domain/Message.java
+++ src/main/java/com/ciklum/domain/Message.java
@@ -1,12 +1,91 @@
 package com.ciklum.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
 
 /**
  * Created by olegnikitindev on 04.08.2016.
  */
 @Entity
 @Table
-public class Message {
+public class Message implements Serializable {
+
+    private static final long serialVersionUID = 6293512124184L;
+
+    @Id
+    @Column
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+
+    @ManyToOne(optional = false)
+    private User sender;
+
+    @ManyToOne(optional = false)
+    private User recipient;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Message message = (Message) o;
+
+        if (!id.equals(message.id)) return false;
+        if (!text.equals(message.text)) return false;
+        if (!sender.equals(message.sender)) return false;
+        return recipient.equals(message.recipient);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + text.hashCode();
+        result = 31 * result + sender.hashCode();
+        result = 31 * result + recipient.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/ciklum/domain/User.java
+++ src/main/java/com/ciklum/domain/User.java
@@ -1,12 +1,77 @@
 package com.ciklum.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
 
 /**
  * Created by olegnikitindev on 04.08.2016.
  */
 @Entity
 @Table
-public class User {
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 6293184L;
+
+    @Id
+    @Column
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Transient
+    private Set<Message> messages;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(Set<Message> messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        return name != null ? name.equals(user.name) : user.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
