b5a9ef697dd3e506058f874e41193153580bf16e
olegnikitindev <olegnikitindev@gmail.com>
2016-08-04 21:45:01 +0300

    Added web config

--- src/main/java/com/ciklum/controller/IndexController.java
+++ src/main/java/com/ciklum/controller/IndexController.java
@@ -1,10 +1,17 @@
 package com.ciklum.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by olegnikitindev on 04.08.2016.
  */
 @Controller
 public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
 }
---
+++ src/main/webapp/WEB-INF/views/errorPages/404.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>404</title>
+</head>
+<body>
+<h1 style="text-align: center">Page not found</h1>
+</body>
+</html>
